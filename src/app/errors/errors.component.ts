1f8ec1a2aa1aad3f5824dada1dfaa092060b13ba
Diksha <dkshprshr92@gmail.com>
2018-10-24 11:03:05 +0530

    Added error handling

---
+++ src/app/errors/errors.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Errors } from '../models/errors.model';
+
+@Component({
+  selector: 'app-errors',
+  templateUrl: './errors.component.html',
+  styleUrls: ['./errors.component.css']
+})
+export class ErrorsComponent {
+
+  formattedErrors: Array<string> = [];
+
+  @Input()
+  set errors(errorList: Errors) {
+    this.formattedErrors = Object.keys(errorList.errors || {})
+      .map(key => `${key} ${errorList.errors[key]}`);
+  }
+
+  get errorList() { return this.formattedErrors; }
+
+}
